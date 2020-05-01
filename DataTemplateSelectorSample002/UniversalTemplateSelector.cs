c8afc248892ac0316d32679f9ee718b041aa8422
Bill Chung <bill_chungiii@hotmail.com>
2020-05-02 00:30:57 +0800

    Add Sample002

---
+++ DataTemplateSelectorSample002/UniversalTemplateSelector.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace DataTemplateSelectorSample002
+{
+    public class UniversalTemplateSelector : DataTemplateSelector
+    {
+        public string Path { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {            
+            var source = new ResourceDictionary { Source = new Uri(Path, UriKind.RelativeOrAbsolute) };
+            var attribute = item?.GetType().GetCustomAttribute<DescriptionAttribute>();
+            if (attribute != null && source.Contains(attribute.Description))
+            {
+                return source[attribute.Description] as DataTemplate;
+            }
+            return null;
+        }
+    }
+}
---
+++ DataTemplateSelectorSample002/ViewModels/Person.cs
@@ -0,0 +1,51 @@
+﻿using BindingLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace DataTemplateSelectorSample002.ViewModels
+{
+    [Description("personTemplate")]
+    public class Person : NotifyPropertyBase
+    {
+        private string _name;
+        [SuppressMessage("ReSharper", "ArrangeAccessorOwnerBody")]
+        public string Name
+        {
+            get => _name;
+            set => SetProperty(ref _name, value);
+        }
+
+
+        private int _age;
+        public int Age
+        {
+            get => _age;
+            set => SetProperty(ref _age, value);
+        }
+    }
+
+    [Description ("carTemplate")]
+    public class Car : NotifyPropertyBase
+    {
+        private string _brand;
+       
+        public string Brand
+        {
+            get => _brand;
+            set => SetProperty(ref _brand, value);
+        }
+
+        private Color _color;
+        public Color Color 
+        {
+            get => _color; 
+            set => SetProperty(ref _color , value); 
+        }
+    }
+}
